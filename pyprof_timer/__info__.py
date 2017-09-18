25a3f56293d6741f9df2f3d67ecccb0dd4b47a24
RussellLuo <luopeng.he@gmail.com>
2017-09-18 17:29:06 +0800

    Bump version to 0.0.3

--- pyprof_timer/__info__.py
+++ pyprof_timer/__info__.py
@@ -1,6 +1,6 @@
 """A timer for profiling a Python function or snippet."""
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = 'RussellLuo'
 __email__ = 'luopeng.he@gmail.com'
 __license__ = 'MIT'
