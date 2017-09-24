24b9592141d4d527d9d3a568f3438aee792d9950
RussellLuo <luopeng.he@gmail.com>
2017-09-24 15:15:54 +0800

    Rename __info__.py to __about__.py

--- pyprof_timer/__about__.py
+++ pyprof_timer/__about__.py
@@ -1,6 +1,11 @@
 """A timer for profiling a Python function or snippet."""
 
+__title__ = 'pyprof-timer'
+__uri__ = 'https://github.com/RussellLuo/pyprof-timer'
+
 __version__ = '0.0.4'
+
 __author__ = 'RussellLuo'
 __email__ = 'luopeng.he@gmail.com'
+
 __license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -5,27 +5,25 @@ import os
 from setuptools import setup, find_packages
 
 
-def get_info(name):
-    basedir = os.path.dirname(__file__)
-    with open(os.path.join(basedir, 'pyprof_timer/__info__.py')) as f:
-        locals = {}
-        exec(f.read(), locals)
-        return locals[name]
+# Manually extract the __about__
+about = {}
+with open('pyprof_timer/__about__.py') as f:
+    exec(f.read(), about)
 
 
 setup(
-    name='pyprof-timer',
-    version=get_info('__version__'),
-    author=get_info('__author__'),
-    author_email=get_info('__email__'),
-    maintainer=get_info('__author__'),
-    maintainer_email=get_info('__email__'),
+    name=about['__title__'],
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__email__'],
+    maintainer=about['__author__'],
+    maintainer_email=about['__email__'],
     keywords='Profiling, Timer, Python',
-    description=get_info('__doc__'),
-    license=get_info('__license__'),
-    long_description=get_info('__doc__'),
+    description=about['__doc__'],
+    license=about['__license__'],
+    long_description=about['__doc__'],
     packages=find_packages(exclude=['tests']),
-    url='https://github.com/RussellLuo/pyprof-timer',
+    url=about['__uri__'],
     install_requires=[
         'monotonic==1.3',
         'six==1.10.0',
